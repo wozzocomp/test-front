7f7811140b523a817660cbc9aa28e05c85ca3736
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-05 10:37:59 +0200

    filter refactored to filter by id too

--- src/actions/artist/index.js
+++ src/actions/artist/index.js
@@ -1,9 +1,19 @@
 import { apolloQuery } from '../../utils/ApiWrapper';
 import artistQueries from './artistQueries';
+import artistMutation from './artistMutation';
+import { WRONG_PARAMS } from '../../utils/constants';
 
-const getArtists = ({ name, active, deleted }) =>
+const formatForSave = (artist) => ({
+  _id: artist._id,
+  name: artist.name,
+  description: artist.description,
+  active: artist.active,
+  deleted: artist.deleted,
+});
+
+export const getArtists = ({ _id, name, active, deleted }) =>
   new Promise((resolve, reject) => {
-    apolloQuery(artistQueries.artists, { name, active, deleted })
+    apolloQuery(artistQueries.artists, { _id, name, active, deleted })
       .then((response) => {
         resolve(response.data.artists);
       })
