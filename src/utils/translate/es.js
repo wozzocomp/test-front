cf949f826c0ffea5fa9c0cdf2d62707720f84467
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-06 11:35:58 +0200

    musical gender tarnslations added.

--- src/utils/translate/es.js
+++ src/utils/translate/es.js
@@ -2,6 +2,7 @@ import artist from './es/artist.json';
 import common from './es/common.json';
 import error from './es/error.json';
 import language from './es/language.json';
+import musicGender from './es/musicGender.json';
 import navbar from './es/navbar.json';
 import user from './es/user.json';
 import userRole from './es/userRole.json';
@@ -11,6 +12,7 @@ const translations = {
   common,
   error,
   language,
+  musicGender,
   navbar,
   user,
   userRole,
