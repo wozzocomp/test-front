95979a75217add808abba15cab84e009eeabc17d
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-11 17:02:55 +0200

    query formatted

--- src/actions/song/songQueries.js
+++ src/actions/song/songQueries.js
@@ -2,8 +2,22 @@ import gql from 'graphql-tag';
 import song from '../../utils/types/song';
 
 const songs = gql`
-query songs($_id: ID, $name: String, $artistId: ID, $genreId: ID, $album: String $active: Boolean, $deleted: Boolean) {
-  songs (_id: $_id, name: $name, artistId: $artistId, genreId: $genreId, album: $album, active: $active, deleted: $deleted) {
+query songs(
+  $_id: ID,
+  $name: String, 
+  $artistId: ID,
+  $genreId: ID,
+  $album: String,
+  $active: Boolean, 
+  $deleted: Boolean) {
+  songs (
+    _id: $_id, 
+    name: $name, 
+    artistId: $artistId, 
+    genreId: $genreId, 
+    album: $album, 
+    active: $active, 
+    deleted: $deleted) {
     ${song}
   }
 }`;
