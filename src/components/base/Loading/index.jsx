87d8719b0b3ac591507ad486d4b6e1ac9f87f5ab
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-13 16:33:25 +0200

    Icon added to the index.

--- src/components/base/Loading/index.jsx
+++ src/components/base/Loading/index.jsx
@@ -3,6 +3,7 @@ import './index.scss';
 
 const Loading = () => (
   <div className="loading-container fadein">
+    <i className="fas fa-music-alt" />
     <div className="loader fadein" />
     <div className="loader2 fadein" />
     <div className="loader3 fadein" />
