9d93ecd5718deb9c5ac935c9516d37050cc77ab9
mpalao-wozzo <miki.palao@wozzo.es>
2020-08-17 09:28:21 +0200

    music card layout finished.

--- src/components/base/MusicCard/index.jsx
+++ src/components/base/MusicCard/index.jsx
@@ -3,13 +3,24 @@ import { Audio } from '@wozzocomp/base-comps';
 import PropTypes from 'prop-types';
 import './index.scss';
 
-const MusicCard = ({ img, alt, audio }) => (
+const MusicCard = ({ img, alt, audio, song, artist, genre }) => (
   <div className="music-card">
-    <div className="music-card-img">
-      <img src={img} alt={alt} />
+    <div className="music-card-front">
+      <div className="music-card-front-content">
+        <h1>Song: {song}</h1>
+        <p> Artist: {artist}</p>
+        <p>Genre: {genre}</p>
+      </div>
     </div>
-    <div className="music-card-audio">
-      <Audio file={audio} />
+    <div className="music-card-back">
+      <div className="music-card-back-content">
+        <div className="music-card-img">
+          <img src={img} alt={alt} />
+        </div>
+        <div className="music-card-audio">
+          <Audio file={audio} />
+        </div>
+      </div>
     </div>
   </div>
 );
@@ -18,12 +29,18 @@ MusicCard.defaultProps = {
   img: '',
   alt: '',
   audio: '',
+  song: '',
+  artist: '',
+  genre: '',
 };
 
 MusicCard.propTypes = {
   img: PropTypes.string,
   alt: PropTypes.string,
   audio: PropTypes.string,
+  song: PropTypes.string,
+  artist: PropTypes.string,
+  genre: PropTypes.string,
 };
 
 export default MusicCard;
